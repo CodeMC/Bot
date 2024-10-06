7e4c312aab5acaa1f9c52c97811c54c5026b6a93
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-10-05 20:39:22 -0500

    Remove Unused JavaContinuation
    Removed `suspend` Modifiers from Kotlin API

--- src/main/java/io/codemc/bot/utils/APIUtil.java
+++ src/main/java/io/codemc/bot/utils/APIUtil.java
@@ -3,12 +3,9 @@ package io.codemc.bot.utils;
 import io.codemc.api.Generator;
 import io.codemc.api.jenkins.JenkinsAPI;
 import io.codemc.api.nexus.NexusAPI;
-import io.codemc.bot.JavaContinuation;
 import net.dv8tion.jda.api.interactions.InteractionHook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.concurrent.CompletableFuture;
 
 public class APIUtil {
 
@@ -21,107 +18,88 @@ public class APIUtil {
         return Generator.createPassword(PASSWORD_SIZE);
     }
 
-    public static void createNexus(InteractionHook hook, String username, String password) {
-        CompletableFuture<Boolean> nexus = new CompletableFuture<>();
-        nexus.whenCompleteAsync((success, ex) -> {
-            if(success != null && !success){
-                if (hook != null)
-                    CommandUtil.EmbedReply.from(hook)
-                            .error("Failed to create Nexus Repository!")
-                            .send();
-                
-                if (ex != null)
-                    LOGGER.error("Failed to create Nexus Repository for {}!", username, ex);
-            }
-        });
-        NexusAPI.createNexus(username, password, JavaContinuation.create(nexus));
+    public static boolean createNexus(InteractionHook hook, String username, String password) {
+        boolean success = NexusAPI.createNexus(username, password);
+        if (!success) {
+            if (hook != null)
+                CommandUtil.EmbedReply.from(hook)
+                        .error("Failed to create Nexus Repository!")
+                        .send();
+
+            LOGGER.error("Failed to create Nexus Repository for {}!", username);
+            return false;
+        }
+
+        LOGGER.info("Successfully created Nexus Repository for {}!", username);
+        return true;
     }
 
-    public static void createJenkinsJob(InteractionHook hook, String username, String password, String project, String repoLink) {
-        CompletableFuture<Boolean> isFreestyle = new CompletableFuture<>();
-        isFreestyle.whenCompleteAsync((freestyle, ex) -> {
-            if (freestyle == null) {
-                CommandUtil.EmbedReply.from(hook)
-                        .error("Failed to determine if the project is freestyle!")
-                        .send();
-                
-                if (ex != null)
-                    LOGGER.error("Failed to determine if the project is freestyle for {}!", username, ex);
-                else
-                    LOGGER.error("Failed to determine if the project is freestyle for {}! (No Errors)", username);
+    public static boolean createJenkinsJob(InteractionHook hook, String username, String password, String project, String repoLink) {
+        if (!JenkinsAPI.getJenkinsUser(username).isEmpty()) {
+            CommandUtil.EmbedReply.from(hook)
+                    .error("Jenkins User for " + username + " already exists!")
+                    .send();
 
-                return;
-            }
+            LOGGER.error("Jenkins User for {} already exists!", username);
+            return false;
+        }
 
-            boolean userSuccess = JenkinsAPI.createJenkinsUser(username, password);
-            if (!userSuccess) {
-                CommandUtil.EmbedReply.from(hook)
-                        .error("Failed to create Jenkins User for " + username + "!")
-                        .send();
+        boolean userSuccess = JenkinsAPI.createJenkinsUser(username, password);
+        if (!userSuccess) {
+            CommandUtil.EmbedReply.from(hook)
+                    .error("Failed to create Jenkins User for " + username + "!")
+                    .send();
 
-                LOGGER.error("Failed to create Jenkins User for {}!", username);
-                return;
-            }
+            LOGGER.error("Failed to create Jenkins User for {}!", username);
+            return false;
+        }
 
-            boolean jobSuccess = JenkinsAPI.createJenkinsJob(username, project, repoLink, freestyle);
-            if (!jobSuccess) {
-                CommandUtil.EmbedReply.from(hook)
-                        .error("Failed to create Jenkins Job '" + project + "' for " + username + "!")
-                        .send();
+        boolean freestyle = JenkinsAPI.isFreestyle(repoLink);
+        boolean jobSuccess = JenkinsAPI.createJenkinsJob(username, project, repoLink, freestyle);
+        if (!jobSuccess) {
+            CommandUtil.EmbedReply.from(hook)
+                    .error("Failed to create Jenkins Job '" + project + "' for " + username + "!")
+                    .send();
 
-                LOGGER.error("Failed to create Jenkins Job '{}' for {}!", project, username);
-                return;
-            }
+            LOGGER.error("Failed to create Jenkins Job '{}' for {}!", project, username);
+            return false;
+        }
 
-            boolean triggerBuild = JenkinsAPI.triggerBuild(username, project);
-            if (!triggerBuild) {
-                CommandUtil.EmbedReply.from(hook)
-                        .error("Failed to trigger Jenkins Build for " + username + "!")
-                        .send();
+        boolean triggerBuild = JenkinsAPI.triggerBuild(username, project);
+        if (!triggerBuild) {
+            CommandUtil.EmbedReply.from(hook)
+                    .error("Failed to trigger Jenkins Build for " + username + "!")
+                    .send();
 
-                LOGGER.error("Failed to trigger Jenkins Build for {}!", username);
-            }
-        });
-        JenkinsAPI.isFreestyle(repoLink, JavaContinuation.create(isFreestyle));
+            LOGGER.error("Failed to trigger Jenkins Build for {}!", username);
+            return false;
+        }
+
+        LOGGER.info("Successfully created Jenkins Job '{}' for {}!", project, username);
+        return true;
     }
 
-    public static void changePassword(InteractionHook hook, String username, String newPassword) {
-        CompletableFuture<Boolean> changeNexusPassword = new CompletableFuture<>();
-        changeNexusPassword.whenCompleteAsync((nexusSuccess, nexusEx) -> {
-            if (nexusSuccess == null || !nexusSuccess){
-                if (hook != null)
-                    CommandUtil.EmbedReply.from(hook)
-                            .error("Failed to change Nexus Repository password!")
-                            .send();
+    public static boolean changePassword(InteractionHook hook, String username, String newPassword) {
+        boolean jenkinsSuccess = JenkinsAPI.changeJenkinsPassword(username, newPassword);
+        if (!jenkinsSuccess) {
+            CommandUtil.EmbedReply.from(hook)
+                    .error("Failed to change Jenkins Password for " + username + "!")
+                    .send();
 
-                if (nexusEx != null)
-                    LOGGER.error("Failed to change Nexus Repository password for {}!", username, nexusEx);
-                else
-                    LOGGER.error("Failed to change Nexus Repository password for {}! (No Errors)", username);
-            }
+            LOGGER.error("Failed to change Jenkins Password for {}!", username);
+            return false;
+        }
 
-            CompletableFuture<Boolean> changeJenkinsPassword = new CompletableFuture<>();
-            changeJenkinsPassword.whenCompleteAsync((jenkinsSuccess, jenkinsEx) -> {
-                if (jenkinsSuccess == null || !jenkinsSuccess){
-                    if (hook != null)
-                        CommandUtil.EmbedReply.from(hook)
-                                .error("Failed to change Jenkins password!")
-                                .send();
+        boolean nexusSuccess = NexusAPI.changeNexusPassword(username, newPassword);
+        if (!nexusSuccess) {
+            CommandUtil.EmbedReply.from(hook)
+                    .error("Failed to change Nexus Password for " + username + "!")
+                    .send();
 
-                    if (jenkinsEx != null)
-                        LOGGER.error("Failed to change Jenkins password for {}!", username, jenkinsEx);
-                    else
-                        LOGGER.error("Failed to change Jenkins password for {}! (No Errors)", username);
-                }
-
-                if (hook != null)
-                    CommandUtil.EmbedReply.from(hook)
-                            .success("Successfully changed your password!")
-                            .send();
-            });
-            JenkinsAPI.changeJenkinsPassword(username, newPassword, JavaContinuation.create(changeJenkinsPassword));
-        });
-        NexusAPI.changeNexusPassword(username, newPassword, JavaContinuation.create(changeNexusPassword));
+            LOGGER.error("Failed to change Nexus Password for {}!", username);
+            return false;
+        }
+        return true;
     }
 
 }
