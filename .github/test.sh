86c6f8b01296f962157a24967fb075fc7fa267db
Gregory Mitchell <me@gmitch215.xyz>
2024-11-18 14:00:46 -0600

    Update test.sh

--- .github/test.sh
+++ .github/test.sh
@@ -2,4 +2,7 @@
 
 # Setup API Server
 git clone https://github.com/CodeMC/API build/tmp/CodeMC-API
+chmod +x ./build/tmp/CodeMC-API/.github/test.sh
+
+# Run API Server
 ./build/tmp/CodeMC-API/.github/test.sh
