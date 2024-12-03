0f97c919f33c19e472ddfa7b7dd83467acb0a05c
Gregory Mitchell <me@gmitch215.xyz>
2024-12-03 22:36:32 +0000

    Remove Unused Imports

--- src/main/java/io/codemc/bot/utils/ApplicationHandler.java
+++ src/main/java/io/codemc/bot/utils/ApplicationHandler.java
@@ -33,8 +33,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class ApplicationHandler{
 
